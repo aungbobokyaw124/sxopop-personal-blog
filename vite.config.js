0291ae4386f111c0bdc398fad5d000b3a3fa46cb
aungbobokyaw.com <141192688+aungbobokyaw124@users.noreply.github.com>
2026-09-05 16:40:15 +0630

    Configure Tailwind processing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const { default: tailwindcss } = await import('@tailwindcss/vite')
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
